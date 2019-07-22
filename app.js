14cea10fd3bbec436b2f36ffd2c8fe43a30ae339
shahzad <shazad.02@gmail.com>
2019-07-22 03:03:37 -0700

    1st

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express'),
+    passport = require('passport'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    User = require('./user'),
+    LocalStrategy = require('passport-local');
+var app=express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'topsecreat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/auth1');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+//   });
+  
+// passport.deserializeUser(function(id, done) {
+//     User.getUserById(id, function(err, user) {
+//       done(err, user);
+//     });
+//   });
+
+app.post('/hello',(req,res)=>{
+    console.log('requestedd');
+    res.send('lineman1');
+});
+
+app.post('/auth',passport.authenticate('local'),function(req,res){
+    console.log('i am here');
+    console.log(req.user.username);
+    res.send(req.user.username);
+});
+
+app.post('/signup',(req,res)=>{
+    User.register(new User({username:req.body.username}),
+    req.body.password,
+    function(err,user){
+        if(err){
+            console.log(err);
+            res.json({message:err})
+        }
+        else {
+            console.log(user);
+            res.json({message : user})
+        }
+    });
+});
+
+app.listen(4000,()=>{
+    console.log('auth server started');
+});
---
+++ user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var passportLocal = require('passport-local-mongoose');
+
+var userSchema = new mongoose.Schema({
+    username : String,
+    password : String
+});
+
+userSchema.plugin(passportLocal);
+module.exports = mongoose.model('user',userSchema);
